90fca368271b720d6037ec0b919ab48ac138e4d1
Darren Liu <darrrenliu@gmail.com>
2025-03-29 03:23:35 -0400

    aaa'

--- validationbs.ino
+++ validationbs.ino
@@ -1,8 +1,8 @@
 const int ledPin = 2;  // D2 (GPIO 2) for blinking LED
-const int inputPins[] = {32, 33, 34, 35, 36, 39};  // Input pins
+const int inputPins[] = {34, 35, 36, 39, 25, 26, 22, 23, 21, 19, 16, 4};  // Input pins
 const int numInputs = sizeof(inputPins) / sizeof(inputPins[0]);
 const int numMotors = 20;
-const int motorIDS[numMotors] = {13, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33, 0, 2, 4, 5, 12, 14, 15};
+const int motorIDS[numMotors] = {13, 12, 14, 27, 33, 32, 15, 2, 0, 17, 5, 18};
 
 void setup() {
   pinMode(ledPin, OUTPUT);  
@@ -18,8 +18,20 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH);
-  delay(500);
-  digitalWrite(ledPin, LOW);
-  delay(500);
+  bool inputDetected = false;
+
+  // Check if any input pin is HIGH
+  for (int i = 0; i < numInputs; i++) {
+    if (digitalRead(inputPins[i]) == HIGH) {
+      inputDetected = true;
+      break;  // No need to check further
+    }
+  }
+
+  // Blink LED if input is detected
+  if (inputDetected) {
+    digitalWrite(ledPin, HIGH);
+  }else{
+    digitalWrite(ledPin, LOW);
+  }
 }
